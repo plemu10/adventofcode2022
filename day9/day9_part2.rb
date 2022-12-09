b2dc256ab7bfdc4ff73ce9a16680eee525cd68d7
Paul Lemieux <plemu10@gmail.com>
2022-12-09 16:25:08 -0600

    day9 part 2 solution

--- day9/day9_part2.rb
+++ day9/day9_part2.rb
@@ -1,8 +1,8 @@
 #Part 1 
 input = File.readlines("input.txt").each {|line| line.chomp!}		#read the file in and strip out the new line characters
 
-head_x, head_y, tail_x, tail_y = 0, 0, 0, 0
 tail_positions = []
+knots = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
 
 input.each do |line|
     motion = line.split(' ')
@@ -10,62 +10,74 @@ input.each do |line|
     distance = motion[1].to_i
 
     puts "Current motion is #{line}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    puts "Current head position is (#{head_x}, #{head_y})"
-    puts "Current tail position is (#{tail_x}, #{tail_y})"
-    distance.times do
-        case direction
-        when "U"
-            head_y += 1
-            puts "Move head up, head pos now (#{head_x}, #{head_y})"
-        when "D"
-            head_y -= 1
-            puts "Move head down, head pos now (#{head_x}, #{head_y})"
-        when "L"
-            head_x -= 1
-            puts "Move head left, head pos now (#{head_x}, #{head_y})"
-        when "R"
-            head_x += 1
-            puts "Move head right, head pos now (#{head_x}, #{head_y})"
-        end
+    puts "Current head position is (#{knots[0][0]}, #{knots[0][1]})"
 
-        x_diff = head_x - tail_x
-        y_diff = head_y - tail_y
-        head_pos = [x_diff, y_diff]
-        p head_pos
-        case head_pos
-        when [-2, 2] , [-1, 2] , [-2, 1]
-            puts "Move tail diagonal up-left"
-            tail_x -= 1
-            tail_y += 1
-        when [0, 2]
-            puts "Move tail up"
-            tail_y += 1
-        when [1, 2] , [2, 2] , [2, 1]
-            puts "Move tail diagonal up-right"
-            tail_x += 1
-            tail_y += 1
-        when [-2, 0]
-            puts "Move tail left"
-            tail_x -= 1
-        when [2, 0]
-            puts "Move tail right"
-            tail_x += 1
-        when [-2, -1] , [-2, -2] , [-1, -2]
-            puts "Move tail diagonal down-left"
-            tail_x -= 1
-            tail_y -= 1
-        when [0, -2]
-            puts "Move tail down"
-            tail_y -= 1
-        when [1, -2] , [2, -2] , [2, -1]
-            puts "Move tail diagonal-right"
-            tail_x += 1
-            tail_y -= 1
-        else
-            puts "Tail does not need to move this time"
+    distance.times do                                   #Do for as many time as distance
+        knots.each_with_index do |segment, index|       #Process each segment as if it was it's own head-tail
+            break if index == 9                         #Stop processing because the tail is already updated and nothing comes after
+            #gets.chomp
+            head_x = knots[index][0]                    #head_x is the x coord of the current segment
+            head_y = knots[index][1]                    #head_y is the y coord of the current segment
+            tail_x = knots[index + 1][0]                #tail_X is the x coord of the next segment
+            tail_y = knots[index + 1][1]                #tail_y is the y coord of the next segment
+
+            if index == 0
+                case direction
+                when "U"
+                    head_y += 1
+                    puts "Move #{index}'rd knot up, pos now (#{head_x}, #{head_y})"
+                when "D"
+                    head_y -= 1
+                    puts "Move #{index}'rd knot down, pos now (#{head_x}, #{head_y})"
+                when "L"
+                    head_x -= 1
+                    puts "Move #{index}'rd knot left, pos now (#{head_x}, #{head_y})"
+                when "R"
+                    head_x += 1
+                    puts "Move #{index}'rd knot right, pos now (#{head_x}, #{head_y})"
+                end
+            end
+    
+            head_pos = [head_x - tail_x, head_y - tail_y] #Take the diff between the head and tail to get a position relative to the tail
+            case head_pos
+            when [-2, 2] , [-1, 2] , [-2, 1]
+                puts "Move #{index + 1}'rd knot diagonal up-left"
+                tail_x -= 1
+                tail_y += 1
+            when [0, 2]
+                puts "Move #{index + 1}'rd knot up"
+                tail_y += 1
+            when [1, 2] , [2, 2] , [2, 1]
+                puts "Move #{index + 1}'rd knot diagonal up-right"
+                tail_x += 1
+                tail_y += 1
+            when [-2, 0]
+                puts "Move #{index + 1}'rd knot left"
+                tail_x -= 1
+            when [2, 0]
+                puts "Move #{index + 1}'rd knot right"
+                tail_x += 1
+            when [-2, -1] , [-2, -2] , [-1, -2]
+                puts "Move #{index + 1}'rd knot diagonal down-left"
+                tail_x -= 1
+                tail_y -= 1
+            when [0, -2]
+                puts "Move #{index + 1}'rd knot down"
+                tail_y -= 1
+            when [1, -2] , [2, -2] , [2, -1]
+                puts "Move #{index + 1}'rd knot diagonal-right"
+                tail_x += 1
+                tail_y -= 1
+            else
+                puts "#{index + 1}'rd knot does not need to move this time"
+            end
+            puts "Tail pos is now (#{tail_x}, #{tail_y})"
+            knots[index][0] = head_x         #save the updated head_x coord to the segment
+            knots[index][1] = head_y         #save the updated head_y coord to the segment
+            knots[index + 1][0] = tail_x     #update tail_x which will be the head of the next segment
+            knots[index + 1][1] = tail_y     #update tail_y which will be the head of the next segment
+            tail_positions << [tail_x, tail_y] if index == 8        #If we are on the tail segment add the position to the list
         end
-        puts "Tail pos is now (#{tail_x}, #{tail_y})"
-        tail_positions << [tail_x, tail_y]
     end
 end
 
